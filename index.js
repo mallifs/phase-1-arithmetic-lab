84a0ce7f6812e87c4425e6da9c8892b73ec86481
leon <mallifs@icloud>
2022-11-25 06:47:40 +0000

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// let a = 31;
+// let b = 2;
+// let results = (a * b);
+//  console.log(results)
+const num1 = 31;
+const num2 = 2;
+ const multiply = num1 * num2
 
-// Write your code here
+
+ const random= Math.floor(Math.random()*10)+1;
+
+ const num3 = 104;
+ const num4 = 20;
+ const mod = num3 % num4;
+
+ const max = Math.max( 1,19, 12,20);
